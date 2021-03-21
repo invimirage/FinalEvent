60cb2becb2ba6e17551e2c72011c5c96b92b290c
zrf <zrf1999@qq.com>
2021-03-21 10:46:58 +0800

    init

---
+++ TextAnalysis/model.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: caopeng
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: deamoncao100@gmail.com
+@software:XXXX
+@file: model.py
+@time: 2021/3/21 10:46
+@desc:
+'''
