a248232961d2bb37c5dab8b31acb4c2931c63df3
invi_mirage <zrf1999@qq.com>
2021-04-28 17:00:41 +0800

    long text bert

---
+++ TextAnalysis/long_text_scorer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: zhangruifeng
+@contact: zrf1999@pku.edu.cn
+@file: long_text_scorer.py
+@time: 2021/4/28 13:56
+@github: local 16351726fa15c85f565b7d5fecdf320ea67a72ef
+'''
--- VideoAnalysis/Crawler/spiders/mySpider.py
+++ VideoAnalysis/Crawler/spiders/mySpider.py
@@ -1,5 +1,6 @@
 import scrapy
 import pandas as pd
+import os
 
 # 还没想好要爬什么，现在只需要首帧
 class DataFetcher(scrapy.Spider):
@@ -7,16 +8,22 @@ class DataFetcher(scrapy.Spider):
     allowed_domains = ["adwetec.com"]
     data_folder = "../Data/"
     img_sources = pd.read_csv(
-       data_folder + "kuaishou_data_0421.csv",
+       data_folder + "kuaishou_data_0426.csv",
         encoding="utf-8",
     )
     image_folder = data_folder + 'images/'
-    urls = img_sources["first_frame"]
-    start_urls = [
-        "https://constrain.adwetec.com/material/creative/image/" + url
-        for url in urls
-    ]
-    img_dict = {k: str(v) + '.jpeg' for k, v in zip(start_urls, img_sources["id"])}
+    image_list = list(filter(lambda x: x[-4:] == 'jpeg', os.listdir(image_folder)))
+    ids_already = list(map(lambda x: x.split('.')[0], image_list))
+    img_dict = {}
+    urls = []
+    for id, url in zip(img_sources["id"], img_sources["first_frame"]):
+        if str(id) not in ids_already:
+            target_url = "https://constrain.adwetec.com/material/creative/image/" + url
+            img_dict[target_url] = str(id) + '.jpeg'
+            urls.append(target_url)
+    print('%d images waiting to download' % len(urls))
+    start_urls = urls
+    # img_dict = {k: str(v) + '.jpeg' for k, v in zip(start_urls, img_sources["id"])}
 
     def parse(self, response):
         img_local_path = self.image_folder + self.img_dict[str(response.url)]
